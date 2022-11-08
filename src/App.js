8a19d90b6f209275a575fae2e94dea59dd81edc3
osama aslam <osamaaslam029@gmail.com>
2022-11-08 20:20:37 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      {/* <Home/> */}
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './Navbar.scss'
+import logo from '../../Assets/logo.png'
+import one from '../../Assets/one.png'
+import two from '../../Assets/two.png'
+import three from '../../Assets/three.png'
+import four from '../../Assets/four.png'
+import five from '../../Assets/five.png'
+import six from '../../Assets/six.png'
+import seven from '../../Assets/seven.png'
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className='navbar'>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+      </div> 
+      <div className="green-box">
+        <div className="flex-boxes">
+          <div className="box">
+            <img src={one} alt="" />
+            <div>One</div>
+          </div>
+          <div className="box">
+            <img src={two} alt="" />
+            <div>Two</div>
+          </div>
+          <div className="box">
+            <img src={three} alt="" />
+            <div>Three</div>
+          </div>
+          <div className="box">
+            <img src={four} alt="" />
+            <div>Four</div>
+          </div>
+          <div className="box">
+            <img src={five} alt="" />
+            <div>Five</div>
+          </div>
+          <div className="box">
+            <img src={six} alt="" />
+            <div>Six</div>
+          </div>
+          <div className="box">
+            <img src={seven} alt="" />
+            <div>Seven</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
