722e2916e3233a32c710f3cb35e074b532754724
ZahidGhafoor <zahidghafoor40@gmail.com>
2022-11-08 23:49:43 +0500

    osama slider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.scss';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar'
+import HomeSlider from './Components/Slider/Slider';
 // https://www.figma.com/file/uu1D1793s1wGMh4XxZiKAY/Madrasa.io?node-id=0%3A1
 function App() {
   return (
     <>
-      <Navbar/>
-      {/* <Home/> */}
-    
+      {/* <Navbar /> */}
+      <HomeSlider/>
     </>
   );
 }
---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Slider from "react-slick";
+import "./Slider.scss";
+
+import home from "../../Assets/home.svg";
+
+const arr = [
+  {
+    img: home,
+    heading: "Lorem Ipsum",
+  },
+  {
+    img: home,
+    heading: "Lorem",
+  },
+  {
+    img: home,
+    heading: "Ipsum",
+  },
+  {
+    img: home,
+    heading: "Ipsum",
+  },
+  {
+    img: home,
+    heading: "Lorem",
+  },
+  {
+    img: home,
+    heading: "Ipsum",
+  },
+  {
+    img: home,
+    heading: "Ipsum",
+  },
+];
+const HomeSlider = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+  return (
+    <div className="parent">
+      <Slider {...settings}>
+        {arr.map((data, i) => {
+          return (
+            <div className="slide_parent" key={i}>
+              <div>
+                <img src={data.img} alt="Icons" />
+              </div>
+              <div className="text">{data.heading}</div>
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  );
+};
+
+export default HomeSlider;
