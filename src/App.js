cfe5839b41d80eae2059bd2371f91a02507b65c0
osama aslam <osamaaslam029@gmail.com>
2022-11-08 20:25:14 +0500

    figma link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar'
-
+// https://www.figma.com/file/uu1D1793s1wGMh4XxZiKAY/Madrasa.io?node-id=0%3A1
 function App() {
   return (
     <>
