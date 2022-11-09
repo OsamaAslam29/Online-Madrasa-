f776414b1518b70a323caeea39ebfbf82d09bf7e
ZahidGhafoor <zahidghafoor40@gmail.com>
2022-11-10 00:53:55 +0500

    absolute images

--- src/Components/ImagesContainer/Images.jsx
+++ src/Components/ImagesContainer/Images.jsx
@@ -4,60 +4,88 @@ import home from '../../Assets/homes.png'
 import image from '../../Assets/image.jpg'
 import ImageCard from './ImageCard'
 
+
+import large from "../../Assets/large.svg";
+import small from "../../Assets/small.svg";
+
 const Images = () => {
     return (
-        <>
-            <ImageCard/>
-            
-            <div className='Image-container'>
-                <div className="flex-card">
-                    <div className="image-card">
-                        <div className="logo">
-                            <img src={home} alt="" />
-                        </div>
-                        <div>
-                            <a href="#">Lorem Ipsum</a>
-                        </div>
-                        <span>Lorem Ipsum is simply dummy text of <br />the printing and typesetting industry.<br /> Lorem Ipsum is simply dummy</span>
-                    </div>
-                    <div className="image-card">
-                        <div className="logo">
-                            <img src={home} alt="" />
-                        </div>
-                        <div>
-                            <a href="#">Lorem Ipsum</a>
-                        </div>
-                        <span>Lorem Ipsum is simply dummy text of <br />the printing and typesetting industry.<br /> Lorem Ipsum is simply dummy</span>
-                    </div>
-                </div>
-                <div className="middle-image">
-                    <img src={image} alt="" />
-                </div>
-                <div className="flex-card">
-                    <div className="image-card">
-                        <div className="logo">
-                            <img src={home} alt="" />
-                        </div>
-                        <div>
-                            <a href="#">Lorem Ipsum</a>
-                        </div>
-                        <span>Lorem Ipsum is simply dummy text of <br />the printing and typesetting industry.<br /> Lorem Ipsum is simply dummy</span>
-                    </div>
-                    <div className="image-card">
-                        <div className="logo">
-                            <img src={home} alt="" />
-                        </div>
-                        <div>
-                            <a href="#">Lorem Ipsum</a>
-                        </div>
-                        <span>Lorem Ipsum is simply dummy text of <br />the printing and typesetting industry.<br /> Lorem Ipsum is simply dummy</span>
-                    </div>
-                </div>
-
+      <>
+        <div className="images__main">
+          <div className="absolute__images">
+            <div className="large">
+              <img src={large} alt="" />
+              <div className="small">
+                <img src={small} alt="" />
+              </div>
             </div>
+          </div>
+          <ImageCard />
 
-        </>
-    )
+          <div className="Image-container">
+            <div className="flex-card">
+              <div className="image-card">
+                <div className="logo">
+                  <img src={home} alt="" />
+                </div>
+                <div>
+                  <a href="#">Lorem Ipsum</a>
+                </div>
+                <span>
+                  Lorem Ipsum is simply dummy text of <br />
+                  the printing and typesetting industry.
+                  <br /> Lorem Ipsum is simply dummy
+                </span>
+              </div>
+              <div className="image-card">
+                <div className="logo">
+                  <img src={home} alt="" />
+                </div>
+                <div>
+                  <a href="#">Lorem Ipsum</a>
+                </div>
+                <span>
+                  Lorem Ipsum is simply dummy text of <br />
+                  the printing and typesetting industry.
+                  <br /> Lorem Ipsum is simply dummy
+                </span>
+              </div>
+            </div>
+            <div className="middle-image">
+              <img src={image} alt="" />
+            </div>
+            <div className="flex-card">
+              <div className="image-card">
+                <div className="logo">
+                  <img src={home} alt="" />
+                </div>
+                <div>
+                  <a href="#">Lorem Ipsum</a>
+                </div>
+                <span>
+                  Lorem Ipsum is simply dummy text of <br />
+                  the printing and typesetting industry.
+                  <br /> Lorem Ipsum is simply dummy
+                </span>
+              </div>
+              <div className="image-card">
+                <div className="logo">
+                  <img src={home} alt="" />
+                </div>
+                <div>
+                  <a href="#">Lorem Ipsum</a>
+                </div>
+                <span>
+                  Lorem Ipsum is simply dummy text of <br />
+                  the printing and typesetting industry.
+                  <br /> Lorem Ipsum is simply dummy
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
 
 export default Images
