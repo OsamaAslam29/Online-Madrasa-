baf51316eaceb8362a6c928add07c46ee52c3c63
osama aslam <osamaaslam029@gmail.com>
2022-12-09 01:26:49 +0500

    manage width of  cards section

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -7,75 +7,80 @@ import card from "../../Assets/Icon.png"
 
 const Cards = () => {
     return (
-        <div className='card-container' id='team'>
-            <div className="text">
-                <h2>Join the Online Quran Community to Learn, Read, and Listen.</h2>
-                <div>As one of the leading online institutes for teaching the Quran online, <br /> we offer the best educational experiences for our students and the world.</div>
+        <>
+            <div>
+                <div className='card-container'>
+                    <div className="text">
+                        <h2>Join the Online Quran Community to Learn, Read, and Listen.</h2>
+                        <div>As one of the leading online institutes for teaching the Quran online, <br /> we offer the best educational experiences for our students and the world.</div>
+                    </div>
+                    <div className="cards">
+                        <div className="card">
+                            <div className="img">
+                                <img src={card} style={{ width: "100%" }} />
+                            </div>
+                            <div className="bio">
+                                <h5>Name</h5>
+                                <div className="title">Desigination</div>
+                                <span>Lorem Ipsum is simply dummy text of the printing and typesetting</span>
+                            </div>
+                            <div className="icons">
+                                <img src={fb} alt="" />
+                                <img src={ints} alt="" />
+                                <img src={link} alt="" />
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="img" >
+                                <img src={card} style={{ width: "100%" }} />
+                            </div>
+                            <div className="bio">
+                                <h5>Name</h5>
+                                <div className="title">Desigination</div>
+                                <span>Lorem Ipsum is simply dummy text of the printing and typesetting</span>
+                            </div>
+                            <div className="icons">
+                                <img src={fb} alt="" />
+                                <img src={ints} alt="" />
+                                <img src={link} alt="" />
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="img" >
+                                <img src={card} style={{ width: "100%" }} />
+                            </div>
+                            <div className="bio">
+                                <h5>Name</h5>
+                                <div className="title">Desigination</div>
+                                <span>Lorem Ipsum is simply dummy text of the printing and typesetting</span>
+                            </div>
+                            <div className="icons">
+                                <img src={fb} alt="" />
+                                <img src={ints} alt="" />
+                                <img src={link} alt="" />
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="img" >
+                                <img src={card} style={{ width: "100%" }} />
+                            </div>
+                            <div className="bio">
+                                <h5>Name</h5>
+                                <div className="title">Desigination</div>
+                                <span>Lorem Ipsum is simply dummy text of the printing and typesetting</span>
+                            </div>
+                            <div className="icons">
+                                <img src={fb} alt="" />
+                                <img src={ints} alt="" />
+                                <img src={link} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="contact"></div>
             </div>
-            <div className="cards">
-                <div className="card">
-                    <div className="img">
-                        <img src={card} style={{ width: "100%" }} />
-                    </div>
-                    <div className="bio">
-                        <h5>Name</h5>
-                        <div className="title">Desigination</div>
-                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting</span>
-                    </div>
-                    <div className="icons">
-                        <img src={fb} alt="" />
-                        <img src={ints} alt="" />
-                        <img src={link} alt="" />
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img" >
-                        <img src={card} style={{ width: "100%" }} />
-                    </div>
-                    <div className="bio">
-                        <h5>Name</h5>
-                        <div className="title">Desigination</div>
-                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting</span>
-                    </div>
-                    <div className="icons">
-                        <img src={fb} alt="" />
-                        <img src={ints} alt="" />
-                        <img src={link} alt="" />
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img" >
-                        <img src={card} style={{ width: "100%" }} />
-                    </div>
-                    <div className="bio">
-                        <h5>Name</h5>
-                        <div className="title">Desigination</div>
-                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting</span>
-                    </div>
-                    <div className="icons">
-                        <img src={fb} alt="" />
-                        <img src={ints} alt="" />
-                        <img src={link} alt="" />
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img" >
-                        <img src={card} style={{ width: "100%" }} />
-                    </div>
-                    <div className="bio">
-                        <h5>Name</h5>
-                        <div className="title">Desigination</div>
-                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting</span>
-                    </div>
-                    <div className="icons">
-                        <img src={fb} alt="" />
-                        <img src={ints} alt="" />
-                        <img src={link} alt="" />
-                    </div>
-                </div>
-            </div>
-            <div id='contact'></div>
-        </div>
+        </>
+
     )
 }
 
